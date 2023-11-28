4c5aab894101b376a2f50245fccfbda933df4fc9
Dalpil <47555+dalpil@users.noreply.github.com>
2023-11-28 15:04:27 +0000

    Changes

--- multitone.py
+++ multitone.py
@@ -2,16 +2,29 @@ from itertools import chain
 import socket
 import sys
 
-c = socket.create_connection((sys.argv[1], 9100))
+from PIL import Image, ImageOps
 
-width = 512
-height = 100
+# c = socket.create_connection((sys.argv[1], 9100))
 
-c.sendall(bytes([0x1d, 0x28, 0x4b, 0x02, 0x00, 0x61, 1])) # Single head energizing
-c.sendall(bytes([0x1d, 0x28, 0x4b, 0x02, 0x00, 0x32, 1])) # Lowest speed
+# c.sendall(bytes([0x1d, 0x28, 0x4b, 0x02, 0x00, 0x61, 1])) # Single head energizing
+# c.sendall(bytes([0x1d, 0x28, 0x4b, 0x02, 0x00, 0x32, 1])) # Lowest speed
+
+image = Image.open(sys.argv[1])
+image = image.convert('L')
+
+if image.width > 512:
+    ratio = 512 / image.width
+    image = image.resize((int(image.width * ratio), int(image.height * ratio)))
+
+width = image.width
+height = image.height
+
+print(width, height)
+
+image = bytes([p // 17 for p in image.tobytes()])
 
 # A gradient from level 0 to level 15
-image = bytes(list(chain.from_iterable([x] * 31 + [0] for x in range(16))) * height)
+# image = bytes(list(chain.from_iterable([x] * 31 + [0] for x in range(16))) * height)
 
 colors = [
     [0x00] * 64 * height,
@@ -24,22 +37,25 @@ for index, pixel in enumerate(image):
     x = index % width
     y = index // width
 
+    if not pixel:
+        continue
+
     if pixel & 0b1000:
-        colors[0][64 * y + x // 8] |= 1 << (7 - index % 8)
+        plane = 0
 
     if pixel & 0b0100:
-        colors[1][64 * y + x // 8] |= 1 << (7 - index % 8)
+        plane = 1
 
     if pixel & 0b0010:
-        colors[2][64 * y + x // 8] |= 1 << (7 - index % 8)
+        plane = 2
 
     if pixel & 0b0001:
-        colors[3][64 * y + x // 8] |= 1 << (7 - index % 8)
+        plane = 3
+
+    colors[plane][64 * y + x // 8] |= 1 << (7 - index % 8)
 
 for i in range(4):
     print(colors[i][:64])
-
-colors = [bytes(x) for x in colors]
 
 for index, color_code in enumerate(range(49, 53)):
     data = bytes([
@@ -59,9 +75,9 @@ for index, color_code in enumerate(range(49, 53)):
 
         (width >> 0) & 0xff, (width >> 8) & 0xff,
         (height >> 0) & 0xff, (height >> 8) & 0xff,
-    ]) + colors[index]
+    ]) + bytes(colors[index])
 
-    c.sendall(data)
+    # c.sendall(data)
 
-c.sendall(bytes([0x1d, 0x28, 0x4c, 0x02, 0x00, 0x30, 2])) # Print stored data
-c.sendall(bytes([0x1d, 0x56, 65, 100])) # Feed and cut
+# c.sendall(bytes([0x1d, 0x28, 0x4c, 0x02, 0x00, 0x30, 2])) # Print stored data
+# c.sendall(bytes([0x1d, 0x56, 65, 100])) # Feed and cut
