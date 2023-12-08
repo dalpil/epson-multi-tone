84250587f95a5fbd43dbb6034b6f5d8e9187d6da
Dalpil <47555+dalpil@users.noreply.github.com>
2023-12-08 14:44:49 +0000

    Code cleanup, less sharpening

--- src/multitone/multitone.py
+++ src/multitone/multitone.py
@@ -9,43 +9,6 @@ from PIL import Image, ImageOps, ImageEnhance
 logging.basicConfig(level=logging.WARNING)
 logging.getLogger('PIL').setLevel(logging.WARNING)
 
-DITHER_KERNEL = (
-    (1, 0, 0.5423),
-    (2, 0, 0.0533),
-
-    (-2, 1, 0.0246),
-    (-1, 1, 0.2191),
-    (0, 1, 0.4715),
-    (1, 1, -0.0023),
-    (2, 1, -0.1241),
-
-    (-2, 2, -0.0065),
-    (-1, 2, -0.0692),
-    (0, 2, 0.0168),
-    (1, 2, -0.0952),
-    (2, 2, -0.0304),
-)
-
-palette = [0, 36, 72, 109, 145, 182, 218, 255]
-
-lut = [
-    4,  # 0
-    5,  # 1
-    6,  # 2
-    6,  # 3
-    7,  # 4
-    7,  # 5
-    7,  # 6
-    8,  # 7
-    8,  # 8
-    9,  # 9
-    10, # 10
-    11, # 11
-    12, # 12
-    13, # 13
-    14, # 14
-    15, # 15
-]
 
 @numba.njit
 def dither(original, diff_map, serpentine, palette):
@@ -58,16 +21,7 @@ def dither(original, diff_map, serpentine, palette):
     for y in range(height):
         for x in range(0, width, direction) if direction > 0 else range(width - 1, -1, direction):
             old_pixel = input[y, x]
-
-            old_distance = 255
-            new_pixel = 0
-            for each in palette:
-                distance = abs(each - old_pixel)
-
-                if distance < old_distance:
-                    old_distance = distance
-                    new_pixel = each
-
+            new_pixel = palette[np.argmin(np.abs(palette - old_pixel))]
             quantization_error = old_pixel - new_pixel
             output[y, x] = new_pixel
 
@@ -79,21 +33,19 @@ def dither(original, diff_map, serpentine, palette):
                 xn, yn = x + dx, y + dy
 
                 if (0 <= xn < width) and (0 <= yn < height):
-                    input[yn, xn] = max(0, min(255, input[yn, xn] + round(quantization_error * diffusion_coefficient)))
+                    input[yn, xn] = input[yn, xn] + round(quantization_error * diffusion_coefficient)
 
-            if serpentine and ((direction > 0 and x >= (width - 1)) or (direction < 0 and x <= 0)):
-                direction *= -1
+        if serpentine:
+            direction *= -1
 
     return output
 
-def nbytes(bits):
-    return (bits + 8 - 1) // 8
 
 @click.command(context_settings={'show_default': True})
 @click.option('--output-file', type=click.Path(), required=True, help="The binary file to send to the Epson printer")
 @click.option('--output-image', type=click.Path())
 @click.option('--num-lines', default=100, help='Should be less than half of 415')
-@click.option('--sharpen', default=4.0)
+@click.option('--sharpen', default=2.0)
 @click.argument('image', type=click.File(mode='rb'))
 def main(image, output_file, output_image, num_lines, sharpen):
     image = Image.open(image)
@@ -103,34 +55,47 @@ def main(image, output_file, output_image, num_lines, sharpen):
         image = image.resize((int(image.width * ratio), int(image.height * ratio)))
 
     image = image.convert('L')
-
     image = ImageEnhance.Sharpness(image)
     image = image.enhance(sharpen)
 
     width = image.width
-    width_nbytes = nbytes(width)
+    width_nbytes = (width + 8 - 1) // 8
     height = image.height
 
-    numba_palette = numba.typed.List()
-    for x in palette:
-        numba_palette.append(x)
+    dither_kernel = (
+        (1, 0, 0.5423),
+        (2, 0, 0.0533),
 
-    image = dither(np.array(image), DITHER_KERNEL, True, numba_palette)
+        (-2, 1, 0.0246),
+        (-1, 1, 0.2191),
+        (0, 1, 0.4715),
+        (1, 1, -0.0023),
+        (2, 1, -0.1241),
+
+        (-2, 2, -0.0065),
+        (-1, 2, -0.0692),
+        (0, 2, 0.0168),
+        (1, 2, -0.0952),
+        (2, 2, -0.0304),
+    )
+    palette = [0, 36, 72, 109, 145, 182, 218, 255]
+    image = dither(np.array(image), dither_kernel, True, np.array(palette))
     image = Image.fromarray(np.uint8(image), "L")
 
     if output_image:
         image.save(output_image)
 
     image = ImageOps.invert(image)
-    image = bytes([p // 17 for p in image.tobytes()])
-    image = [lut[x] for x in image]
 
+    # Apply LUT transformation to map pixel values to levels that the printer can reproduce on paper.
+    lut = [4, 5, 6, 6, 7, 7, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15]
+    image = [lut[p // 17] for p in image.tobytes()]
 
     output = b''
     output += bytes([0x1d, 0x28, 0x4b, 0x02, 0x00, 0x61, 1]) # Single head energizing
     output += bytes([0x1d, 0x28, 0x4b, 0x02, 0x00, 0x32, 1]) # Lowest speed
 
-    for chunk_num, chunk in enumerate(range(0, width_nbytes * height, width_nbytes * num_lines)):
+    for chunk_num in range(height // num_lines + 1):
         for color_index, color_code in enumerate(range(49, 53)):
             bitplane = [0x00] * width_nbytes * num_lines
             image_offset = chunk_num * (width * num_lines)
